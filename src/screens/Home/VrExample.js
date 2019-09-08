bbb546f67d3e55e7cf1c85bd04a4eb64e46f077e
Mohammad Affan <b4affan@gmail.com>
2019-09-08 20:29:28 +0700

    example curved bottom header

--- src/screens/Home/VrExample.js
+++ src/screens/Home/VrExample.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { PanoramaView } from "@lightbase/react-native-panorama-view";
-import { View, StatusBar, Dimensions, SafeAreaView, Image } from "react-native";
-import { Header } from "react-native-elements";
+import { View, Dimensions, SafeAreaView, Image } from "react-native";
+// import { Header } from "react-native-elements";
+// import { Header } from "react-native-elements";
+import { Header, CurvedBottomHeader } from "../../components";
 
 const width = Dimensions.get("window").width;
 const vr = require("../../../assets/img/vr.png");
@@ -11,19 +13,22 @@ export default class VrExample extends Component {
         const url = Image.resolveAssetSource(vr);
         return (
             <SafeAreaView>
-                <StatusBar barStyle="light-content" hidden={false} showHideTransition={true} animated={true} />
                 <Header
+                    statusBarProps={{ barStyle: "light-content", hidden: false, showHideTransition: true, animated: true }}
                     placement="left"
+                    containerStyle={{ backgroundColor: 'red' }}
                     leftComponent={{ icon: 'menu', color: '#fff' }}
                     centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
                     rightComponent={{ icon: 'home', color: '#fff' }}
                 />
                 <View style={{ flex: 1 }}>
+                    <CurvedBottomHeader />
                     <PanoramaView
                         style={{ height: 230 }}
                         dimensions={{ height: 230, width: width }}
                         inputType="mono"
-                        imageUrl="https://raw.githubusercontent.com/googlevr/gvr-android-sdk/master/assets/panoramas/testRoom1_2kMono.jpg"
+                        // imageUrl="https://raw.githubusercontent.com/googlevr/gvr-android-sdk/master/assets/panoramas/testRoom1_2kMono.jpg"
+                        imageUrl={url.uri}
                     />
                 </View>
             </SafeAreaView>
