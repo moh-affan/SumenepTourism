6fbe2d458ec5332d1aadeef38b02d6d79a834a03
Mohammad Affan <b4affan@gmail.com>
2019-09-12 11:24:32 +0700

    change title font, use bottom curved header add card view

--- src/screens/Home/VrExample.js
+++ src/screens/Home/VrExample.js
@@ -1,37 +1,38 @@
 import React, { Component } from "react";
 import { PanoramaView } from "@lightbase/react-native-panorama-view";
-import { View, Dimensions, SafeAreaView, Image } from "react-native";
+import { View, Dimensions, SafeAreaView, Image, TouchableNativeFeedback } from "react-native";
 // import { Header } from "react-native-elements";
-// import { Header } from "react-native-elements";
-import { Header, CurvedBottomHeader } from "../../components";
+import { Card, Icon, Button } from "react-native-elements";
+import { CurvedBottomHeader } from "../../components";
 
 const width = Dimensions.get("window").width;
 const vr = require("../../../assets/img/vr.png");
 
 export default class VrExample extends Component {
-    render() {
-        const url = Image.resolveAssetSource(vr);
-        return (
-            <SafeAreaView>
-                <Header
-                    statusBarProps={{ barStyle: "light-content", hidden: false, showHideTransition: true, animated: true }}
-                    placement="left"
-                    containerStyle={{ backgroundColor: 'red' }}
-                    leftComponent={{ icon: 'menu', color: '#fff' }}
-                    centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
-                    rightComponent={{ icon: 'home', color: '#fff' }}
-                />
-                <View style={{ flex: 1 }}>
-                    <CurvedBottomHeader />
-                    <PanoramaView
-                        style={{ height: 230 }}
-                        dimensions={{ height: 230, width: width }}
-                        inputType="mono"
-                        // imageUrl="https://raw.githubusercontent.com/googlevr/gvr-android-sdk/master/assets/panoramas/testRoom1_2kMono.jpg"
-                        imageUrl={url.uri}
-                    />
-                </View>
-            </SafeAreaView>
-        );
-    }
+  render() {
+    const url = Image.resolveAssetSource(vr);
+    
+    return (
+      <SafeAreaView>
+        <CurvedBottomHeader
+          statusBarProps={{ barStyle: "light-content", hidden: false, showHideTransition: true, animated: true }}
+          placement="center"
+          containerStyle={{ backgroundColor: '#E64A19' }}
+          leftComponent={<Button type="solid" buttonStyle={{borderRadius: 50}} containerStyle={{borderRadius: 50}} raised icon={<Icon name="menu" color="white" />} />}
+          centerComponent={{ text: 'Visit Sumenep', style: { color: '#fff', fontFamily: 'Lobster', fontSize: 24 } }}
+          height={50} />
+        <View style={{ flex: 1, marginTop: -50, paddingLeft: 14, paddingRight: 14 }}>
+          <Card containerStyle={{ height: 242, padding: 5, borderRadius: 5 }}>
+            <PanoramaView
+              style={{ height: 230 }}
+              dimensions={{ height: 230, width: width }}
+              inputType="mono"
+              // imageUrl="https://raw.githubusercontent.com/googlevr/gvr-android-sdk/master/assets/panoramas/testRoom1_2kMono.jpg"
+              imageUrl={url.uri}
+            />
+          </Card>
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
